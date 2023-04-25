8f0ebb0468742d1750c6661899cbb95e7ca1fb1c
lochmidth <alphanogun@gmail.com>
2023-04-25 16:13:47 +0300

    Initial commit

---
+++ I am Rich UI/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  I am Rich UI
+//
+//  Created by Alphan Ogün on 23.04.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Color(.systemTeal)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("I am Rich!")
+                    .font(.system(size: 40))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                Image("diamond")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200.0, height: 200.0, alignment: .center)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ I am Rich UI/I_am_Rich_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_am_Rich_UIApp.swift
+//  I am Rich UI
+//
+//  Created by Alphan Ogün on 23.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct I_am_Rich_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
